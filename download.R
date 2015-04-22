95c87252932601a1db12cb32e159b82e64b54cd4
Simon Crase <simon@greenweaves.nz>
2015-04-23 07:28:26 +1200

    download.daily now handles dates correctly

--- download.R
+++ download.R
@@ -23,16 +23,21 @@ library(data.table)
 if(!file.exists("data"))
   dir.create("data")
 
-download.daily<-function() {
-  download.file("http://192.168.1.2/log_daily.csv",destfile="data/log_daily.csv",mode="wb")
+
+download.daily<-function(url="http://192.168.1.2") {
+  download.file(file.path(url,"log_daily.csv"),destfile="data/log_daily.csv",mode="wb")
   data=as.data.table(read.csv("data/log_daily.csv",skip=3,header=FALSE,sep="\t", skipNul=TRUE,strip.white=TRUE))
-  setnames(data,names(data),c("D","Power","Duration"))
+  data$Date<-as.Date(data$V1)
+  data$V1=NULL
+  setcolorder(data, c("Date", "V2", "V3"))
+  setnames(data,names(data),c("Date","Power","Duration"))
   data
 }
 
 download.5min<-function() {
   download.file("http://192.168.1.2/log_5min.csv",destfile="data/log_5min.csv",mode="wb")
   data=as.data.table(read.csv("data/log_5min.csv",skip=3,header=FALSE,sep="\t", skipNul=TRUE,strip.white=TRUE))
+  data$Date<-as.Date(data$V1)
   setnames(data,names(data),c("DT","Power"))
   data
 }
